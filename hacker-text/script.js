5d94e96b5520acadf3ec8b5a90b1aa3e3a65afb0
kyleCS50 <kyledanielfrancis@gmail.com>
2023-02-03 16:24:02 +1300

    Made hacker text

---
+++ hacker-text/script.js
@@ -0,0 +1,18 @@
+const letters = "abcdefghijklmnopqrstuvwxyz";
+document.querySelector("h1").onmouseover = (event) => {
+  const startWord = event.target.innerText;
+  let count = 0;
+  const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, i) => {
+        if (i < count) {
+          return startWord[i];
+        }
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+    if (count > startWord.length) clearInterval(interval);
+    count += 1 / 10;
+  }, 30);
+};
